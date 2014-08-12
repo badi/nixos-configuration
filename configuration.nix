adeecfb8f4b290f1a91823a04a9cba6e72b8abd2
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-08-12 08:20:51 -0400

    take daily zfs snapshots

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,35 @@
 
 { config, pkgs, ... }:
 
+
+let
+
+  zfsSnapshotKeepCount = "90";
+  zfsSnapshotPools = "tank";
+
+  zfsSnapshotScript = pkgs.writeScript "zfs-snapshot" ''
+    ${pkgs.coreutils}/bin/echo "==================================="
+    ${pkgs.coreutils}/bin/date
+    pool=$1
+    keep=$2
+
+    zfs=${pkgs.linuxPackages.zfs}/sbin/zfs
+    grep=${pkgs.gnugrep}/bin/grep
+    sort=${pkgs.coreutils}/bin/sort
+    head=${pkgs.coreutils}/bin/head
+    tail=${pkgs.coreutils}/bin/tail
+    xargs=${pkgs.findutils}/bin/xargs
+
+    $zfs snapshot -r $pool@$(date +%F-%T)
+    $zfs list -t snapshot -o name \
+         | $grep tank@ \
+	 | $sort -r \
+	 | $tail -n +$keep \
+	 | $xargs -n 1 $zfs destroy -r
+    '';
+
+in
+
 {
 
   nixpkgs.config.allowUnfree = true;
@@ -37,7 +66,6 @@
     file
     lsof
     which
-    zfstools
 
     # network tools
     lshw wget
@@ -105,16 +133,20 @@
 
 
   # define cronjobs
-  # /-------------------- minute
-  # | /------------------ hour
-  # | | /---------------- day of month
-  # | | | /-------------- month
-  # | | | | /------------ day of week
-  # | | | | |
-  # * * * * *
+  services.cron.enable = true;
+  services.cron.systemCronJobs = [
 
-  # services.cron.systemCronJobs = [
-  #   ""
+  #    /-------------------- minute
+  #    | /------------------ hour
+  #    | | /---------------- day of month
+  #    | | | /-------------- month
+  #    | | | | /------------ day of week
+  #    | | | | |
+  #    * * * * *
+
+     # every day at 6am
+      "* 6 * * * root ${zfsSnapshotScript} ${zfsSnapshotPools} ${zfsSnapshotKeepCount} >>/var/log/zfs-snapshot.log 2>&1"
+    ];
 
 
 }
