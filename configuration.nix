59d4453c267bdc0cb197cfcc7a2e39937974f5b2
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-08-13 14:19:31 -0400

    fix frequency bug in zfs snapshot crontab

--- configuration.nix
+++ configuration.nix
@@ -185,7 +185,7 @@ in
   #    * * * * *
 
      # every day at 6am
-      "* 6 * * * root ${zfsSnapshotScript} ${zfsSnapshotPools} ${zfsSnapshotKeepCount} >>/var/log/zfs-snapshot.log 2>&1"
+      "0 6 * * * root ${zfsSnapshotScript} ${zfsSnapshotPools} ${zfsSnapshotKeepCount} >>/var/log/zfs-snapshot.log 2>&1"
 
       # every day at 6:30 am
       "30 6 * * * root bash -l -c 'nix-channel --update'"
